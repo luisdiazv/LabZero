b871663dd4e9de98cd2200d659b9a53f7aa721df
sergio <saparadaa@unal.edu.co>
2024-12-18 20:38:22 -0500

    cambios minimos en el home con front

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="home-container">
-      <img src="/CRUD.png" alt="CRUD" style={{ width: '100%', height: 'auto' }} />
+      <img
+        src="/CRUD.png"
+        alt="CRUD"
+        className="hero-image" // Añadí una clase para poder estilizarla desde el CSS
+      />
       <div className="home-header">
         <h1>Presentado por <b>PlanifiKlub</b>:</h1>
         <ul className="list">
@@ -19,14 +23,11 @@ const Home = () => {
             <img className="person-img" src="/chara.png" alt="CRUD" style={{ width: '80%', height: 'auto' }} />
             <h3><strong>Maria Jose Jara Herrera</strong></h3>
           </div>
-
           <div className='person-container'>
             <img className="person-img" src="/sergio.png" alt="CRUD" style={{ width: '80%', height: 'auto' }} />
             <h3><strong>Sergio Alexander Parada Amarillo</strong></h3>
           </div>
-
         </ul>
-
       </div>
     </div>
   );
